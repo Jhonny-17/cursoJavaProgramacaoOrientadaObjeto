dc43aaac479693bf820028c2bc1181c2a8dc97c1
Jhonny Monteiro <jhonny.silva@sankhya.com.brgit config --global user.email jhonny.silva@sankhya.com.br>
2023-06-30 17:00:23 -0300

    first commit

--- resolvendo_um_problema_sem_orientacao_a_objeto/src/application/exercicio_1.java
+++ resolvendo_um_problema_sem_orientacao_a_objeto/src/application/exercicio_1.java
@@ -26,11 +26,9 @@ public class exercicio_1 {
 		y.c = sc.nextDouble();
 		
 		
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		double areaX = x.area();
 		
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
