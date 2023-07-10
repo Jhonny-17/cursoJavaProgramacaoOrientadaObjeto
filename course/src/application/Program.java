d25ca45ebddbb04c7eb7c7a19910a5830d8cc714
Jhonny Monteiro <jhonny.silva@sankhya.com.brgit config --global user.email jhonny.silva@sankhya.com.br>
2023-07-10 13:20:50 -0300

    first commit

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -12,15 +12,19 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Product product = new Product();
+		Product p = new Product();
+		
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
-
+		double price = sc.nextDouble();
+		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Update price: " + product.getPrice());
 		System.out.println();
 		System.out.println("Product data: " + product);
 		
