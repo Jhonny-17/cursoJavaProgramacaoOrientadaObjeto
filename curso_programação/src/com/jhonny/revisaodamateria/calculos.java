33ecb8bceed870f634920511677a24fff4b060ea
Jhonny Monteiro <jhonny.silva@sankhya.com.brgit config --global user.email jhonny.silva@sankhya.com.br>
2023-07-21 11:59:18 -0300

    Operações

---
+++ curso_programação/src/com/jhonny/revisaodamateria/calculos.java
@@ -0,0 +1,31 @@
+package com.jhonny.revisaodamateria;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class calculos {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		double y = sc.nextDouble();
+		
+		double mult = x * y;
+		double divis = x/y;
+		double som = x + y;
+		
+		System.out.printf("Operações: " + mult + ";" + divis + ";" + som);
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
